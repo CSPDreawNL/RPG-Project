d352793a7f28acd4bcd0abc033d6d51699592dc6
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-03-11 05:15:18 +0100

    Added lots of new things
    Added BOXOPHOBIC (for Skybox)
    Added TransitionScene
    Added start up for the saving system
    Added portals for traveling between scenes

---
+++ RPG Project/Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultQuickSave = "QuickSave";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                GetComponent<SavingSystem>().Save(defaultQuickSave);
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                GetComponent<SavingSystem>().Load(defaultQuickSave);
+            }
+        }
+    }
+}
+
